cc450bf1e67a2ead5dbbb84e9930f89e2decafb1
yelmach <yassineelmach0@gmail.com>
2024-10-25 13:08:02 +0100

    create database

--- database/setupDb.go
+++ database/setupDb.go
@@ -1,2 +1,36 @@
 package database
 
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func SetupDb() (*sql.DB, error) {
+	// it creates a sql.DB object that acts as a connection pool.
+	// The database connections are automatically managed behind the scenes.
+	db, err := sql.Open("sqlite3", "./database.db")
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %v", err)
+	}
+
+	// check if the connection establish
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("error connecting to database: %v", err)
+	}
+
+	// read schema design database from .sql file
+	schema, err := os.ReadFile("schema.sql")
+	if err != nil {
+		return nil, fmt.Errorf("error reading schema.sgl: %v", err)
+	}
+
+	// creating tables
+	if _, err := db.Exec(string(schema)); err != nil {
+		return nil, fmt.Errorf("error executing schema.sgl: %v", err)
+	}
+
+	return db, nil
+}
