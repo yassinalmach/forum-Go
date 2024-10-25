2b2bf1f02b358ef385337fd8e0ed0badb80781b8
yelmach <yassineelmach0@gmail.com>
2024-10-25 18:24:32 +0100

    front-end files

--- main.go
+++ main.go
@@ -1,2 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"forum/database"
+	"forum/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db, err := database.SetupDb()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	mux := routes.SetupRoutes()
+	
+	fmt.Println("Server running on: http://localhost:8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
